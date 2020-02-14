6a386019ea933b80ea7ec631d65570c5ea9da0b4
HausCloud <hauscloud.me@gmail.com>
2020-02-14 19:12:04 +0000

    Add simple overlay

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -6,56 +6,56 @@ window.addEventListener('load', function() {
 	backspeed: 40,
 	loop: true
     };
-
+    let overlayDisplay = document.getElementById('overlay')
     new Typed('#anttitle div h2', options);
-
 
     /* Register plugins for GreenSock */
     gsap.registerPlugin(ScrollToPlugin);
 
 
     /* Smooth scroll for navbar */
-    document.querySelector('#typicalbackground #navbar ul li:first-child a').addEventListener("click", function(e) {
+    document.querySelector('#typicalbackground #navbar ul li:first-child a').addEventListener('click', function(e) {
 	e.preventDefault();
-	gsap.to(document.body, {duration: 0.8, scrollTo: {y: "#about", autoKill: false }})
+	gsap.to(document.body, {duration: 0.8, scrollTo: {y: '#about', autoKill: false }})
     });
 
-    document.querySelector('#typicalbackground #navbar ul li:nth-child(3)').addEventListener("click", function(e) {
+    document.querySelector('#typicalbackground #navbar ul li:nth-child(3)').addEventListener('click', function(e) {
 	e.preventDefault();
-	gsap.to(document.body, {duration: 0.8, scrollTo: {y: "#projects", autoKill: false }})
+	gsap.to(document.body, {duration: 0.8, scrollTo: {y: '#projects', autoKill: false }})
     });
 
-    document.querySelector('#typicalbackground #navbar ul li:last-child').addEventListener("click", function(e) {
+    document.querySelector('#typicalbackground #navbar ul li:last-child').addEventListener('click', function(e) {
 	e.preventDefault();
-	gsap.to(document.body, {duration: 0.8, scrollTo: {y: "#contact", autoKill: false }})
+	gsap.to(document.body, {duration: 0.8, scrollTo: {y: '#contact', autoKill: false }})
     });
 
-    gsap.from("#anttitle", {duration: 2, opacity: 0});
+    gsap.from('#anttitle', {duration: 2, opacity: 0});
     
-    gsap.from(".navitem", {duration: 0.8, opacity: 0, y: -70, stagger: 0.25});
+    gsap.from('.navitem', {duration: 0.8, opacity: 0, y: -70, stagger: 0.25});
     
+
+    /* Add listener event for each project to open overlay on click */
+    let projects = document.querySelectorAll('#projects > div > div > a')
+
+    projects.forEach(function(item, index) {
+	item.addEventListener('click', function(e) {
+	    e.preventDefault();
+	    overlayDisplay.style.display = 'block';
+	});
+    });
+
     /* Open links for contact section */
     document.querySelector('#hclinkedin').addEventListener('click', function (e) {
 	e.preventDefault();
-    	window.open("https://www.linkedin.com/in/hauscloud", "_blank");
+    	window.open('https://www.linkedin.com/in/hauscloud', '_blank');
     });
     document.querySelector('#hcgithub').addEventListener('click', function (e) {
 	e.preventDefault();
-    	window.open("https://www.github.com/hauscloud", "_blank");
+    	window.open('https://www.github.com/hauscloud', '_blank');
     });
     document.querySelector('#hctweet').addEventListener('click', function (e) {
 	e.preventDefault();
-    	window.open("https://www.twitter.com/hauscloud", "_blank");
+    	window.open('https://www.twitter.com/hauscloud', '_blank');
     });
 
-
-    /* Open links for project section */
-    document.querySelector('#projects > div > div:first-child > a').addEventListener('click', function (e) {
-	e.preventDefault();
-    	window.open("https://github.com/hauscloud/c-shell", "_blank");
-    });
-    document.querySelector('#projects > div > div:nth-child(2) > a').addEventListener('click', function (e) {
-	e.preventDefault();
-    	window.open("https://github.com/HausCloud/Hopeful-Cosmos", "_blank");
-    });
 });
