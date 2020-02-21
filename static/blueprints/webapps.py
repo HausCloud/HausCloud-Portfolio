b7752c95b0dc309db59197312d84d48253fcbf99
HausCloud <hauscloud.me@gmail.com>
2020-02-21 02:21:10 +0000

    Add base for app hub

--- static/blueprints/webapps.py
+++ static/blueprints/webapps.py
@@ -1,15 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from uuid import uuid4
 
 webapps = Blueprint('webapps', __name__, url_prefix='/app')
+
+@webapps.route('/hub')
+def menu():
+    return render_template('hub.html', cache_id=uuid4())
 
 @webapps.route('/webapp1')
 def webapps1():
     return '<h1> Hello World! </h1>'
-# from flask import Blueprint, render_template
-# from uuid import uuid4
-
-# webapps = Blueprint('webapps', __name__, url_prefix='/app')
-
-# @webapps.route('/hub')
-# def menu():
-#     return render_template('index.html', cache_id=uuid4())
