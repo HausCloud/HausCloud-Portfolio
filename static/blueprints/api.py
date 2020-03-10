73424f0b0ddd6f1989ef63ba9c0ec1015a2c6a1f
HausCloud <hauscloud.me@gmail.com>
2020-03-10 21:19:20 +0000

    Move file.json

--- static/blueprints/api.py
+++ static/blueprints/api.py
@@ -14,9 +14,9 @@ def googleTranslate():
         return abort(400, 'Request must be a valid JSON object and contain an "application/json" content-type')
     elif 'text' not in result:
         return abort(400, 'JSON must contain text property/attribute')
-
+    
     # Connect to Google API translation client
-    translate_client = translate.Client(credentials=service_account.Credentials.from_service_account_file('.file.json'))
+    translate_client = translate.Client(credentials=service_account.Credentials.from_service_account_file('./static/blueprints/.file.json'))
 
     # Translate text to japanese
     result = translate_client.translate(result['text'], target_language='ja')
