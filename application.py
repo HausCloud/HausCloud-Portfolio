5d7222fde6b5d39b24c22b3284a38503e464e226
HausCloud <hauscloud.me@gmail.com>
2020-04-01 17:48:37 +0000

    Update dependencies for oauth and set up test route

--- application.py
+++ application.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, redirect, Blueprint
+# from functools import wraps
+# import json
+# from os import environ as env
+# from werkzeug.exceptions import HTTPException
+# from dotenv import load_dotenv, find_dotenv
+# from authlib.integrations.flask_client import OAuth
+# from six.moves.urllib.parse import urlencode
+
+from flask import Flask, render_template, redirect, Blueprint, jsonify, session, url_for
 # from flask_cors import CORS
 from static.blueprints.api import api
 from static.blueprints.webapps import webapps
 import uuid
+import os
+
+# oauth = OAuth(app)
 
 application = Flask(__name__)
 # CORS(application)
@@ -17,9 +28,17 @@ def redirectHome():
 def home():
     return render_template('index.html', cache_id=uuid.uuid4())
 
+@application.route('/env_var_test')
+def monkey():
+    my_dict = {'monkey': os.getenv('monkey')}
+
+    return jsonify(my_dict)
+
 @application.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', cache_id=uuid.uuid4()), 404
+
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
