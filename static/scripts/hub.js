4ef5ea36c5eed27138d203ee5d2c7945f5f0269d
HausCloud <hauscloud.me@gmail.com>
2020-04-24 02:32:46 +0000

    Swap out arrow functions and replace foreach

--- static/scripts/hub.js
+++ static/scripts/hub.js
@@ -1,33 +1,33 @@
-window.addEventListener('DOMContentLoaded', () => {
-  // Register plugins
-  gsap.registerPlugin(ScrollToPlugin);
+window.addEventListener('DOMContentLoaded', function() {
+    // Register plugins
+    gsap.registerPlugin(ScrollToPlugin);
 
-  // Variable declaration
-  var sub_window = document.querySelectorAll('.subcontainer');
-  var current_view = 0;
-  var end_view = sub_window.length - 1;
+    // Variable declaration
+    var sub_window = document.querySelectorAll('.subcontainer');
+    var current_view = 0;
+    var end_view = sub_window.length - 1;
 
-  // Bind click event to all path and text elements to follow href
-  sub_window.forEach((element) => {
-    var svg_path = element.querySelector('svg > path');
-    var svg_text = element.querySelector('svg > text');
-    svg_path.addEventListener('click', () => window.open(svg_path.getAttribute('href'), '_blank'));
-    svg_text.addEventListener('click', () => window.open(svg_text.getAttribute('href'), '_blank'));
-  });
-
-  // Bind click event to move left in cloud-container
-  document.getElementById('leftarrow').addEventListener('click', () => {
-    if (current_view > 0) {
-      gsap.to('#cloud-container', { duration: 0.5, scrollTo: sub_window[current_view - 1] });
-      current_view -= 1;
+    // Bind click event to all path and text elements to follow href
+    for (i=0; i<sub_window.length;++i) {
+	var svg_path = sub_window[i].querySelector('svg > path');
+	var svg_text = sub_window[i].querySelector('svg > text');
+	svg_path.addEventListener('click', function() { window.open(svg_path.getAttribute('href'), '_blank');});
+	svg_text.addEventListener('click', function () {window.open(svg_text.getAttribute('href'), '_blank');});
     }
-  });
 
-  // Bind click event to move right in cloud-container
-  document.getElementById('rightarrow').addEventListener('click', () => {
-    if (current_view < end_view) {
-      gsap.to('#cloud-container', { duration: 0.5, scrollTo: sub_window[current_view + 1] });
-      current_view += 1;
-    }
-  });
+    // Bind click event to move left in cloud-container
+    document.getElementById('leftarrow').addEventListener('click', function() {
+	if (current_view > 0) {
+	    gsap.to('#cloud-container', { duration: 0.5, scrollTo: sub_window[current_view - 1] });
+	    current_view -= 1;
+	}
+    });
+
+    // Bind click event to move right in cloud-container
+    document.getElementById('rightarrow').addEventListener('click', function() {
+	if (current_view < end_view) {
+	    gsap.to('#cloud-container', { duration: 0.5, scrollTo: sub_window[current_view + 1] });
+	    current_view += 1;
+	}
+    });
 });
