d8741f61d4579a0be8896dacdac8891d2e97b8f2
HausCloud <hauscloud.me@gmail.com>
2020-05-10 19:52:41 +0000

    Add if to delete certificate each time

--- .platform/hooks/postdeploy/01_ssl_setup_certbot-auto.sh
+++ .platform/hooks/postdeploy/01_ssl_setup_certbot-auto.sh
@@ -14,23 +14,26 @@ fi
 # Increase size of string name for --domains
 sed -i "s/$http_string/$bucket_increase/g" /etc/nginx/nginx.conf
 
-# Install certbot-auto
-wget https://dl.eff.org/certbot-auto
-mv certbot-auto /usr/local/bin/certbot-auto
-chown root /usr/local/bin/certbot-auto
-chmod 0755 /usr/local/bin/certbot-auto
+if certbot-auto delete --cert-name "$CERTBOT_CERTNAME"; then
+    # Install certbot-auto
+    wget https://dl.eff.org/certbot-auto
+    mv certbot-auto /usr/local/bin/certbot-auto
+    chown root /usr/local/bin/certbot-auto
+    chmod 0755 /usr/local/bin/certbot-auto
 
-# Detect OS for bootstrapping for Amazon Linux 2
-# Source: https://medium.com/@andrenakkurt/great-guide-thanks-for-putting-this-together-gifford-nowland-c3ce0ea2455
-sed -i "s/${original_string}/${new_string}/g" /usr/local/bin/certbot-auto
+    # Detect OS for bootstrapping for Amazon Linux 2
+    # Source: https://medium.com/@andrenakkurt/great-guide-thanks-for-putting-this-together-gifford-nowland-c3ce0ea2455
+    sed -i "s/${original_string}/${new_string}/g" /usr/local/bin/certbot-auto
 
-# Install SSL for nginx
-# For apache, replace --nginx with --apache
-if ! /usr/local/bin/certbot-auto --nginx --redirect --cert-name "$CERTBOT_CERT_NAME" -m "$CERTBOT_EMAIL" --domains "$CERTBOT_DOMAIN_LIST" --agree-tos --no-eff-email --keep-until-expiring; then
-    # Workaround to use Python3 for --no-site--packages issue for Python2
-    # Source: https://community.letsencrypt.org/t/how-do-i-specify-the-python-version-when-running-certbot-auto-command/89059
-    USE_PYTHON_3=1 /usr/local/bin/certbot-auto --nginx --redirect --cert-name "$CERTBOT_CERT_NAME" -m "$CERTBOT_EMAIL" --domains "$CERTBOT_DOMAIN_LIST" --agree-tos --no-eff-email --keep-until-expiring
+    # Install SSL for nginx
+    # For apache, replace --nginx with --apache
+    if ! /usr/local/bin/certbot-auto --nginx --redirect --cert-name "$CERTBOT_CERT_NAME" -m "$CERTBOT_EMAIL" --domains "$CERTBOT_DOMAIN_LIST" --agree-tos --no-eff-email --keep-until-expiring; then
+	# Workaround to use Python3 for --no-site--packages issue for Python2
+	# Source: https://community.letsencrypt.org/t/how-do-i-specify-the-python-version-when-running-certbot-auto-command/89059
+	USE_PYTHON_3=1 /usr/local/bin/certbot-auto --nginx --redirect --cert-name "$CERTBOT_CERT_NAME" -m "$CERTBOT_EMAIL" --domains "$CERTBOT_DOMAIN_LIST" --agree-tos --no-eff-email --keep-until-expiring
+    fi
+
+    # Autorenewal
+    echo "0 0,12 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && /usr/local/bin/certbot-auto renew -q" | sudo tee -a /etc/crontab >/dev/null
+
 fi
-
-# Autorenewal
-echo "0 0,12 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && /usr/local/bin/certbot-auto renew -q" | sudo tee -a /etc/crontab >/dev/null
